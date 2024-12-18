5b4858211d2d4d0fb846397070fcc8af49bcaa06
dvenkat285 <116060501+dvenkat285@users.noreply.github.com>
2024-12-18 21:26:23 +0530

    Add project files.

---
+++ MVCAppWithWindowsAuth/Controllers/ManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace MVCAppWithWindowsAuth.Controllers
+{
+    [Authorize(Roles = "EE043\\ManagerGroup")]
+    public class ManagerController : Controller
+    {
+        //[Authorize(Users = "EE043\\Manager1, EE043\\Manager2")]
+        //[Authorize(Roles = "EE043\\ManagerGroup")]
+        public ViewResult Home()
+        {
+            return View();
+        }
+        [AllowAnonymous]//allowed access all
+        public ViewResult Help()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCAppWithWindowsAuth/Controllers/StaffController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace MVCAppWithWindowsAuth.Controllers
+{
+    [Authorize(Roles = "EE043\\StaffGroup")]
+    public class StaffController : Controller
+    {
+        //[Authorize(Users = "EE043\\Staff1, EE043\\Staff2, EE043\\Staff3, EE043\\Staff4")]
+        //[Authorize(Roles = "EE043\\StaffGroup")]
+        public ViewResult Home()
+        {
+            return View();
+        }
+        [AllowAnonymous] //allowed access all
+        public ViewResult Help()
+        {
+            return View();
+        }
+    }
+}
